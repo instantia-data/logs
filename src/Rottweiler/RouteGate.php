53a31cae469f7823a08f7ba04f534f257ed1e045
luis.pinto <luis.pinto@instantia.pt>
2020-05-25 10:47:43 +0100

    suspect ip file append

--- src/Rottweiler/RouteGate.php
+++ src/Rottweiler/RouteGate.php
@@ -19,7 +19,7 @@ class RouteGate
         $suspicious_routes = include(database_path('utils/suspicious_routes.php'));
         foreach ($suspicious_routes as $part) {
             if (strpos($url, $part) !== false) {
-                fwrite(fopen(storage_path('logs/ban_ip.txt'), 'w'), request()->ip() . "\n");
+                fwrite(fopen(storage_path('logs/ban_ip.txt'), 'a'), request()->ip() . "\n");
                 return false;
             }
         }
