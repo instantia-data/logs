9a1b7e323cf1c5e7977ae75178983bd89e65d843
luis.pinto <luis.pinto@instantia.pt>
2020-05-25 10:28:17 +0100

    suspicious routes

--- assets/utils/suspicious_routes.php
+++ assets/utils/suspicious_routes.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'wp-admin', 'magento', 'wp-login', '/backup', '/wordpress', '/old',
-    '/index.php', '.php'
+    'wp-admin', 'magento', 'wp-login', '/backup', '/wordpress', '/old', '/temp', '/dev', '/backup',
+     '/test', '/wp',
+    'index.php', '.php', '.php', 'phpmyadmin', 'cgi-bin', 'autodiscover', 'horde', '/bak', '/v1', '/v2'
 ];
 
